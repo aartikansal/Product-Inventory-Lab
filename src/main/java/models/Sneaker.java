c903c5dc5b26e2f8b50559db80b5b4d9387e7a6a
aartikansal <aartilumb@gmail.com>
2020-05-30 15:32:42 -0400

    part 2 complete

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -5,9 +5,13 @@ public class Sneaker {
     private int id;
     private String name;
     private String brand;
-   private String sport;
+    private String sport;
     private float size;
     private int qty;
     private float price;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
