d05fe3590912f482aaf959cab4a54a5c11aafefb
aartikansal <aartilumb@gmail.com>
2020-05-31 18:43:39 -0400

    pom file edited for JSON

--- src/main/java/utils/CSVUtils.java
+++ src/main/java/utils/CSVUtils.java
@@ -1,29 +1,35 @@
 package utils;
 
+import models.Sneaker;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class CSVUtils {
-        private static final char DEFAULT_SEPARATOR = ',';  // (1)
+public class CSVUtils<inventory> {
+    private static final char DEFAULT_SEPARATOR = ',';  // (1)
 
-        // (2)
-        public static void writeLine(Writer w, List<String> values) throws IOException {
-            boolean first = true;
+    // (2)
+    public static void writeLine(Writer w, List<String> values) throws IOException {
+        boolean first = true;
 
-            StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-            // (3)
-            for (String value : values) {
-                if (!first) {
-                    sb.append(DEFAULT_SEPARATOR);
-                }
-                sb.append(value);
-                first = false;
+        // (3)
+        for (String value : values) {
+            if (!first) {
+                sb.append(DEFAULT_SEPARATOR);
             }
-            sb.append("\n");
-
-            w.append(sb.toString());  // (4)
+            sb.append(value);
+            first = false;
         }
+        sb.append("\n");
+
+        w.append(sb.toString());  // (4)
     }
 
+
+   }
